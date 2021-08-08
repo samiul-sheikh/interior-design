8090906348195c3f513a2120acd0fe9782b18fc3
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-08-09 02:31:01 +0600

    animated navbar complete

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './Components/Home/Home';
+import Contact from './Components/Contact/Contact';
+import Blog from './Components/Blog/Blog';
+import About from './Components/About/About';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/home" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem molestias blanditiis voluptatem earum, itaque dolore odio. Reiciendis porro dolores libero commodi excepturi vero fugiat aliquid ad neque deleniti eligendi adipisci quaerat delectus, asperiores blanditiis quia placeat fuga praesentium dolorum atque ex fugit repellendus! Perferendis dolorem, vel provident excepturi ab alias quae ccorporis quo et fuga voluptas? Quam id accusantium laboriosam aut dolorum quia quidem reprehenderit, enim aliquid commodi placeat natus architecto voluptatem odio tempora, molestias perferendis explicabo officia. Possimus vitae repudiandae quos expedita modi molestias temporibus error praesentium?</h1>
+    </div>
+  );
+};
+
+export default Home;
