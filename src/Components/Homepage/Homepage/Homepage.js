98c825226ac24147215061950552a97739f0ad41
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-08-11 00:21:53 +0600

    start header section

--- src/Components/Homepage/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage/Homepage.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
-      <h1>Samiul Sheikh</h1>
+      <Header></Header>
     </div>
   );
 };
